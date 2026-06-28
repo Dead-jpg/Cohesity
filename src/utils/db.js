14af5225b835e520127ffbd3fc9c46c86e4ffbda
POOJARY JASHITH NEETUPRAKASH <jashithnpoojary@gmail.com>
2026-06-28 20:27:52 +0530

    Configure production setup for Netlify and Render

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001/registrations";
+const API_URL = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/registrations` : "http://localhost:3001/registrations";
 
 export const getRegistrations = async () => {
   const response = await fetch(API_URL);
