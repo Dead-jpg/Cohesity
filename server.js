229166e75ca20eb7ee3ff5f68e1eadcd7b22bd82
POOJARY JASHITH NEETUPRAKASH <jashithnpoojary@gmail.com>
2026-07-02 20:31:16 +0530

    fix: integrate helmet middleware for security headers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import helmet from 'helmet';
 import { logError } from './logger.js';
 
 dotenv.config();
@@ -10,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const DB_FILE = 'db.json';
 
-
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
